ac5d17000c7b7708201fdf8ca39b0bb78254acba
milosky89 <camilo77543@hotmail.com>
2022-09-14 21:50:32 -0500

    Actualizacion front con servicios

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { EmpresaComponent } from './register/empresa/empresa.component';
 import { PersonaComponent } from './register/persona/persona.component';
-import { TipoUsuarioComponent } from './register/tipo-usuario/tipo-usuario.component';
 import { OlvidoContrasenaComponent } from './olvido-contrasena/olvido-contrasena.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
@@ -14,17 +12,13 @@ import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     LoginComponent,
-    EmpresaComponent,
     PersonaComponent,
-    TipoUsuarioComponent,
     OlvidoContrasenaComponent,
   ],
   exports:[
     LoginComponent,
     LoginComponent,
-    EmpresaComponent,
     PersonaComponent,
-    TipoUsuarioComponent,
     OlvidoContrasenaComponent,
   ],
   imports: [
