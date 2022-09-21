578c2cfcf49854dbdf7465d59ba067de60975a7d
milosky89 <camilo77543@hotmail.com>
2022-09-21 10:38:05 -0500

    actualizacion proyecto general

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,36 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
-import { PromesasComponent } from './promesas/promesas.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { MascotaComponent } from './mascota/mascota.component';
 import { AnalisisComponent } from './analisis/analisis.component';
 import { InformacionComponent } from './informacion/informacion.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
 import { NgChartsModule } from 'ng2-charts';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
     PagesComponent,
-    PromesasComponent,
     PerfilComponent,
     MascotaComponent,
     InformacionComponent,
     AnalisisComponent,
-    RxjsComponent,
   ],
   exports:[
     DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
     PagesComponent
   ],
   imports: [
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -2,9 +2,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { PromesasComponent } from './promesas/promesas.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { MascotaComponent } from './mascota/mascota.component';
 import { AnalisisComponent } from './analisis/analisis.component';
@@ -24,9 +21,6 @@ const routes: Routes = [
         {path: 'mascota', component: MascotaComponent,data: {titulo: 'Mis Mascotas'}},
         {path: 'informacion', component: InformacionComponent,data: {titulo: 'Consulta Mascotas'}},
         {path: 'analisis', component: AnalisisComponent,data: {titulo: 'Análisis Gráfico'}},
-        {path: 'progress', component: ProgressComponent,data: {titulo: 'progress'}},
-        {path: 'grafica1', component: Grafica1Component,data: {titulo: 'Dashboard'}},
-        {path: 'promesas', component: PromesasComponent,data: {titulo: 'progress'}},
         {path: '', redirectTo: '/dashboard', pathMatch: 'full',data: {titulo: 'Dashboard'}},
       ]
   },
